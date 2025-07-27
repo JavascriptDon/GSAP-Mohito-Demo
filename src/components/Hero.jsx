ad98864b7e284d1bb9a84ec911efc46d3a5c01fc
Helitha Rupasinghe <hr21don>
2025-07-27 15:37:13 +0100

    updated hero animation for the main cocktain video

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 import { SplitText } from 'gsap/all';
+import { useMediaQuery } from 'react-responsive';
 
 const Hero = () => {
+    const videoRef = useRef()
+    const isMobile = useMediaQuery({maxWidth: 767});
+
     useGSAP(()=> {
         const heroSplit = new SplitText('.title', {type: 'chars,words'});
         const paraSplit = new SplitText('.subtitle', {type: 'lines'});
@@ -35,7 +39,26 @@ const Hero = () => {
 	})
 	.to(".right-leaf", { y: 200 }, 0)
 	.to(".left-leaf", { y: -200 }, 0)
-    }, [])
+
+    const startValue = isMobile ? 'top 50%' : 'center 60%';
+    const endValue = isMobile ? '120% top' : 'bottom top';
+
+    const tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: "video",
+        start: startValue,
+        end: endValue,
+        scrub: true, // ties the animation progress to scroll
+        pin: true, // keeps the video in-place. 
+    },
+    });
+
+    // Animate the video: scale and fade
+    videoRef.current.onloadedmetadata = () => {
+        tl.to(videoRef.current , {
+            currentTime: videoRef.current.duration
+    })}
+},[])
   return (
     <>
     <section id="hero" className='noisy'>
@@ -58,6 +81,10 @@ const Hero = () => {
             </div>
         </div>
     </section>
+
+    <div className='video absolute inset-0'>
+        <video ref={videoRef} src="./videos/output.mp4" muted playsInline preload='auto'/>
+    </div>
     </>
   )
 }
